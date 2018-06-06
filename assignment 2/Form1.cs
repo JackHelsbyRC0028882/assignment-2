c3045f158ba3bf04654746c03a5373640082f762
RC0028882 <RC0028882@CRO-ICF07-22>
2018-06-06 13:59:47 +0100

    Add project files.

---
+++ assignment 2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace assignment_2
+{
+    public partial class Form1 : Form
+    {
+
+        class row
+        {
+            double time;
+            double charge;
+        }
+
+        List<row> Table new = List<row>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "csv Files|".csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try 
+            }       
+        }
+
+    }
+}
